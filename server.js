045a984da007ab933036803da91c2f6f876750e2
andhikagst <andhika.alfahrez@gmail.com>
2026-08-16 19:48:27 +0700

    chore: add Next.js production server

---
+++ server.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { createServer } = require("http");
+const next = require("next");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+
+const app = next({
+  dev: false,
+});
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, () => {
+    console.log(`> Next.js server running on port ${port}`);
+  });
+});
