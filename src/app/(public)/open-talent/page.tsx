ad9d0a4364ad2e0de0667db0fa81cbb2139373c6
rvldrh <rivaldo0347t@gmail.com>
2026-08-18 10:00:12 +0700

    feat(landing): add open talent page

---
+++ src/app/(public)/open-talent/page.tsx
@@ -0,0 +1,5 @@
+import { OpenTalentFormContainer } from "@/src/feature/landing/open-talent/container/OpenTalentFormContainer";
+
+export default function OpenTalentPage() {
+  return <OpenTalentFormContainer />;
+}
