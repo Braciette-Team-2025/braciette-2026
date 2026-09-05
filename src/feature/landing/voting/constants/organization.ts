551af18d1b97fdb8b19bbf0db10d5c0a0fae8c91
rvldrh <rivaldo0347t@gmail.com>
2026-09-05 12:36:06 +0700

    fix(voting): update organization constants

--- src/feature/landing/voting/constants/organization.ts
+++ src/feature/landing/voting/constants/organization.ts
@@ -2,53 +2,143 @@ import { Organization } from "../types/organization";
 
 export const ORGANIZATIONS: Organization[] = [
   // BEM
-  { id: "bem-filkom", categoryId: "bem", name: "BEM Fakultas Ilmu Komputer" },
-  { id: "bem-perikanan", categoryId: "bem", name: "BEM Fakultas Perikanan" },
+  {
+    id: "bem-filkom",
+    categoryId: "bem",
+    name: "BEM Fakultas Ilmu Komputer",
+    status: "accepted",
+  },
+  {
+    id: "bem-perikanan",
+    categoryId: "bem",
+    name: "BEM Fakultas Perikanan",
+    status: "accepted",
+  },
   {
     id: "bem-fisip",
     categoryId: "bem",
     name: "BEM Fakultas Ilmu Politik dan Sosial",
+    status: "accepted",
   },
-  { id: "bem-hukum", categoryId: "bem", name: "BEM Fakultas Hukum" },
-  { id: "bem-teknik", categoryId: "bem", name: "BEM Fakultas Teknik" },
-  { id: "bem-ilkom-2", categoryId: "bem", name: "BEM Fakultas Ilmu Komputer" },
-  { id: "bem-ilkom-3", categoryId: "bem", name: "BEM Fakultas Ilmu Komputer" },
-  { id: "bem-ilkom-4", categoryId: "bem", name: "BEM Fakultas Ilmu Komputer" },
-  { id: "bem-ilkom-5", categoryId: "bem", name: "BEM Fakultas Ilmu Komputer" },
-  { id: "bem-ilkom-6", categoryId: "bem", name: "BEM Fakultas Ilmu Komputer" },
-  { id: "bem-ilkom-7", categoryId: "bem", name: "BEM Fakultas Ilmu Komputer" },
-  { id: "bem-ilkom-8", categoryId: "bem", name: "BEM Fakultas Ilmu Komputer" },
+  {
+    id: "bem-hukum",
+    categoryId: "bem",
+    name: "BEM Fakultas Hukum",
+    status: "accepted",
+  },
+  {
+    id: "bem-teknik",
+    categoryId: "bem",
+    name: "BEM Fakultas Teknik",
+    status: "accepted",
+  },
+  {
+    id: "bem-ilkom-2",
+    categoryId: "bem",
+    name: "BEM Fakultas Ilmu Komputer",
+    status: "accepted",
+  },
+  {
+    id: "bem-ilkom-3",
+    categoryId: "bem",
+    name: "BEM Fakultas Ilmu Komputer",
+    status: "accepted",
+  },
+  {
+    id: "bem-ilkom-4",
+    categoryId: "bem",
+    name: "BEM Fakultas Ilmu Komputer",
+    status: "accepted",
+  },
+  {
+    id: "bem-ilkom-5",
+    categoryId: "bem",
+    name: "BEM Fakultas Ilmu Komputer",
+    status: "accepted",
+  },
+  {
+    id: "bem-ilkom-6",
+    categoryId: "bem",
+    name: "BEM Fakultas Ilmu Komputer",
+    status: "accepted",
+  },
+  {
+    id: "bem-ilkom-7",
+    categoryId: "bem",
+    name: "BEM Fakultas Ilmu Komputer",
+    status: "accepted",
+  },
+  {
+    id: "bem-ilkom-8",
+    categoryId: "bem",
+    name: "BEM Fakultas Ilmu Komputer",
+    status: "accepted",
+  },
 
   // DPM
-  { id: "dpm-filkom", categoryId: "dpm", name: "DPM Fakultas Ilmu Komputer" },
-  { id: "dpm-perikanan", categoryId: "dpm", name: "DPM Fakultas Perikanan" },
+  {
+    id: "dpm-filkom",
+    categoryId: "dpm",
+    name: "DPM Fakultas Ilmu Komputer",
+    status: "accepted",
+  },
+  {
+    id: "dpm-perikanan",
+    categoryId: "dpm",
+    name: "DPM Fakultas Perikanan",
+    status: "accepted",
+  },
   {
     id: "dpm-fisip",
     categoryId: "dpm",
     name: "DPM Fakultas Ilmu Politik dan Sosial",
+    status: "accepted",
   },
-  { id: "dpm-hukum", categoryId: "dpm", name: "DPM Fakultas Hukum" },
+  {
+    id: "dpm-hukum",
+    categoryId: "dpm",
+    name: "DPM Fakultas Hukum",
+    status: "accepted",
+  },
 
   // HIMA
   {
     id: "hima-ilkom",
     categoryId: "hima",
     name: "Himpunan Mahasiswa Ilmu Komputer",
+    status: "accepted",
   },
   {
     id: "hima-sistem-informasi",
     categoryId: "hima",
     name: "Himpunan Mahasiswa Sistem Informasi",
+    status: "accepted",
   },
   {
     id: "hima-teknik-informatika",
     categoryId: "hima",
     name: "Himpunan Mahasiswa Teknik Informatika",
+    status: "accepted",
   },
 
   // UKM
-  { id: "ukm-basket", categoryId: "ukm", name: "UKM Bola Basket" },
-  { id: "ukm-musik", categoryId: "ukm", name: "UKM Musik" },
-  { id: "ukm-fotografi", categoryId: "ukm", name: "UKM Fotografi" },
-  { id: "ukm-pecinta-alam", categoryId: "ukm", name: "UKM Pecinta Alam" },
+  {
+    id: "ukm-basket",
+    categoryId: "ukm",
+    name: "UKM Bola Basket",
+    status: "accepted",
+  },
+  { id: "ukm-musik", categoryId: "ukm", name: "UKM Musik", status: "accepted" },
+  {
+    id: "ukm-fotografi",
+    categoryId: "ukm",
+    name: "UKM Fotografi",
+    status: "accepted",
+  },
+  {
+    id: "ukm-pecinta-alam",
+    categoryId: "ukm",
+    name: "UKM Pecinta Alam",
+    status: "accepted",
+  },
 ];
