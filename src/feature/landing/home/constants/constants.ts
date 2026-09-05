c6faf36afc0e4d37ffd3a06ce7eb6684e0db2e84
andhikagst <andhika.alfahrez@gmail.com>
2026-09-05 14:07:02 +0700

    feat: add faq data and fix timeline data

--- src/feature/landing/home/constants/constants.ts
+++ src/feature/landing/home/constants/constants.ts
@@ -3,23 +3,20 @@ import { FaqItem, TimelineItem, VotingGuideItem } from "../types/types";
 export const FAQ_DATA: FaqItem[] = [
   {
     id: 1,
-    title: "Bagaimana cara melakukan pendaftaran?",
-    desc: "Peserta dapat melakukan pendaftaran melalui formulir yang tersedia di website. Setelah data dan dokumen yang dibutuhkan dikirimkan, tim penyelenggara akan melakukan proses verifikasi dan seleksi.",
+    title:
+      "Apakah setiap akun hanya dapat memberikan satu voting untuk setiap jenis organisasi mahasiswa?",
+    desc: "YA, Satu email UB hanya dapat memberikan satu kali vote untuk tiap jenis organisasi mahasiswa.",
   },
   {
     id: 2,
-    title: "Bagaimana cara melakukan pendaftaran?",
-    desc: "Peserta dapat melakukan pendaftaran melalui formulir yang tersedia di website. Setelah data dan dokumen yang dibutuhkan dikirimkan, tim penyelenggara akan melakukan proses verifikasi dan seleksi.",
+    title:
+      "Apakah untuk nominasi most favorite hanya bersumber dari voting website?",
+    desc: "Ya, nominasi most favorite akan dihitung sesuai dengan jumlah voting terbanyak yang dilakukan oleh seluruh mahasiswa Universitas Brawijaya.",
   },
   {
     id: 3,
-    title: "Bagaimana cara melakukan pendaftaran?",
-    desc: "Peserta dapat melakukan pendaftaran melalui formulir yang tersedia di website. Setelah data dan dokumen yang dibutuhkan dikirimkan, tim penyelenggara akan melakukan proses verifikasi dan seleksi.",
-  },
-  {
-    id: 4,
-    title: "Bagaimana cara melakukan pendaftaran?",
-    desc: "Peserta dapat melakukan pendaftaran melalui formulir yang tersedia di website. Setelah data dan dokumen yang dibutuhkan dikirimkan, tim penyelenggara akan melakukan proses verifikasi dan seleksi.",
+    title: "Apakah akan ada awarding terkait nominasi yang disediakan?",
+    desc: "Pasti ada, awarding akan dilaksanakan pada main event Brawijaya Appreciate 2026.",
   },
 ];
 
@@ -28,31 +25,31 @@ export const TIMELINE_DATA: TimelineItem[] = [
     id: 1,
     title: "Technical Meeting 1",
     date: "7 Sept 2026",
-    desc: "Technical Meeting 1 HIMA & UKM",
+    desc: "Technical Meeting 1 BEM & HIMA",
   },
   {
     id: 2,
     title: "Technical Meeting 2",
     date: "8 Sept 2026",
-    desc: "Technical Meeting 1 BEM & DPM",
+    desc: "Technical Meeting 2 UKM & DPM",
   },
   {
     id: 3,
-    title: "Open Voting",
-    date: "9 Sept 2026",
-    desc: "Periode voting dimulai dari tanggal 1 Oktober hingga 27 Oktober 2025.",
+    title: "Open Submission",
+    date: "9 - 29 Sept 2026",
+    desc: "Periode submission dimulai dari tanggal 9 Sept hingga 29 Sept 2026.",
   },
   {
     id: 4,
-    title: "Open Submission",
-    date: "9 Sept - 23 Okt 2026",
-    desc: "Puncak acara Awarding Brawijaya Appreciate 2026",
+    title: "Open Voting",
+    date: "1 - 22 Okt 2026",
+    desc: "Periode voting dimulai dari tanggal 1 Oktober hingga 22 Oktober 2026.",
   },
   {
     id: 5,
-    title: "Awarding Braciate 2026",
-    date: "30 Okt 2026",
-    desc: "Periode voting dimulai dari tanggal 1 Oktober hingga 27 Oktober 2025.",
+    title: "Main Event Braciate 2026",
+    date: "23 Okt 2026",
+    desc: "Puncak acara Awarding Brawijaya Appreciate 2026",
   },
 ];
 
