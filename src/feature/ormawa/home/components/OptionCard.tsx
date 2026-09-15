75ec08bcc0859a28416edcbfc8bad52cd4321b1a
andhikagst <andhika.alfahrez@gmail.com>
2026-09-15 23:54:09 +0700

    feat: adding booklet link on ormawa landing

--- src/feature/ormawa/home/components/OptionCard.tsx
+++ src/feature/ormawa/home/components/OptionCard.tsx
@@ -9,14 +9,14 @@ export default function OptionCard({
 }: OptionCardProps) {
   const Icon = icon;
   return (
-    <Link href={href} className="group">
-      <div className="py-9 px-6 md:py-12 md:px-8 xl:py-15 xl:px-10 bg-blue-700 border-2 border-yellow-50 rounded-2xl md:rounded-3xl xl:rounded-4xl transition-all duration-300 group-hover:border-yellow-500 group-hover:-translate-y-2 group-hover:shadow-[0_8px_30px_rgba(201,162,39,0.25)]">
-        <div className="flex flex-col gap-3 md:gap-6">
-          <Icon className="w-6 h-6 md:h-8 md:w-8 xl:h-12 xl:w-12 text-yellow-200" />
-          <h1 className="text-yellow-200 text-3xl md:text-4xl xl:text-6xl font-bold">
+    <Link href={href} className="group flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col justify-center py-6 px-4 xl:py-8 xl:px-6 bg-blue-700 border-2 border-yellow-50 rounded-2xl md:rounded-3xl xl:rounded-4xl transition-all duration-300 group-hover:border-yellow-500 group-hover:-translate-y-2 group-hover:shadow-[0_8px_30px_rgba(201,162,39,0.25)]">
+        <div className="flex flex-col gap-2 md:gap-4">
+          <Icon className="size-6 md:size-6 xl:size-8 text-yellow-200" />
+          <h1 className="text-yellow-200 text-2xl md:text-3xl xl:text-4xl font-bold">
             {title}
           </h1>
-          <p className="text-yellow-50 text-sm md:text-md xl:text-xl font-medium">
+          <p className="text-yellow-50 text-xs md:text-sm xl:text-md font-medium">
             {desc}
           </p>
         </div>
--- src/feature/ormawa/home/container/OrmawaHomeContainer.tsx
+++ src/feature/ormawa/home/container/OrmawaHomeContainer.tsx
@@ -1,38 +1,71 @@
+import { LandingButton } from "@/src/feature/landing/home/components/ui/LandingButton";
 import OptionCard from "../components/OptionCard";
 import { OPTIONS } from "../constants/ormawa_home";
+import cover_booklet from "@/public/images/booklet/cover_booklet_real.png";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function OrmawaHomeContainer() {
-  const highlight = "font-sloop text-7xl md:text-7xl xl:text-8xl leading-[0.7]";
+  const highlight = "font-sloop text-5xl md:text-6xl xl:text-7xl leading-[0.7]";
   const DROP_SHADOW = {
     base: "drop-shadow-[0_0_12px_rgba(218,161,17,0.8)]",
     md: "drop-shadow-[0_0_16px_rgba(218,161,17,0.8)]",
     xl: "drop-shadow-[0_0_20px_rgba(218,161,17,0.8)]",
   };
+  const BOOKLET_HREF =
+    "https://drive.google.com/drive/folders/1w-1zWM2Ok8pbcgp4HUQ1OBvBdFIZQSRP";
   return (
-    <main className="py-16 md:py-0 px-8 md:px-16 xl:px-24 flex flex-col gap-6 md:gap-8 justify-center md:min-h-[calc(100vh-105px)] lg:min-h-[calc(100vh-121px)]">
+    <main className="px-8 md:px-12 xl:px-24 flex flex-col gap-4 md:gap-6 justify-center min-h-[calc(100vh-64px)] md:min-h-[calc(100vh-105px)] lg:min-h-[calc(100vh-121px)]">
       <div className="text-center md:text-left">
         <h1
-          className={`pt-4 font-the-seasons text-4xl md:text-5xl xl:text-6xl text-yellow-300 drop-shadow-[0_0_20px_rgba(218,161,17,0.8)] ${DROP_SHADOW.base} md:${DROP_SHADOW.md} xl:${DROP_SHADOW.xl}`}
+          className={`pt-4 font-the-seasons text-2xl md:text-3xl xl:text-4xl text-yellow-300 drop-shadow-[0_0_20px_rgba(218,161,17,0.8)] ${DROP_SHADOW.base} md:${DROP_SHADOW.md} xl:${DROP_SHADOW.xl}`}
         >
           <span className={highlight}>S</span>elamat{" "}
           <span className={highlight}>D</span>atang
         </h1>
-        <p className="font-medium text-lg xl:text-xl">
+        <p className="font-medium text-md xl:text-lg">
           Apa yang kamu butuhkan?
         </p>
       </div>
-      <div className="flex flex-col md:flex-row gap-6">
-        {OPTIONS.map((data) => {
-          return (
-            <OptionCard
-              key={data.title}
-              icon={data.icon}
-              title={data.title}
-              desc={data.desc}
-              href={data.href}
+      <div className="flex flex-col md:flex-row gap-4 md:gap-8 xl:gap-12">
+        <div className="flex flex-col gap-4 md:gap-0 md:justify-between md:flex-1 bg-blue-700 p-4 xl:p-6 border-yellow-50 border-2 rounded-2xl md:rounded-3xl xl:rounded-4xl items-center">
+          <h2
+            className={`font-semibold text-sm lg:text-md xl:text-2xl text-yellow-200 ${DROP_SHADOW.base}`}
+          >
+            Booklet Ormawa
+          </h2>
+          <Link
+            href={BOOKLET_HREF}
+            className={` w-2/3 transform -rotate-3 transition-all duration-300 hover:scale-105 hover:-rotate-1 hidden md:block border-2 border-yellow-300 rounded-lg overflow-hidden ${DROP_SHADOW["md"]}`}
+          >
+            <Image
+              src={cover_booklet}
+              alt="booklet"
+              className="object-cover w-full h-auto drop-shadow-xl"
             />
-          );
-        })}
+          </Link>
+          <div className="w-full">
+            <LandingButton
+              href={BOOKLET_HREF}
+              className="w-full md:text-sm! xl:text-md! px-0! "
+            >
+              Cek Booklet
+            </LandingButton>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 xl:gap-6 md:flex-2 lg:flex-3 xl:flex-4">
+          {OPTIONS.map((data) => {
+            return (
+              <OptionCard
+                key={data.title}
+                icon={data.icon}
+                title={data.title}
+                desc={data.desc}
+                href={data.href}
+              />
+            );
+          })}
+        </div>
       </div>
     </main>
   );
